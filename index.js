558cba803f30a6c6175a75c5ae201b363e8e8843
Semhar Gezai <91860982+SemharGezai@users.noreply.github.com>
2022-03-23 09:24:27 +0000

    users: list/view

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//here's our test data 
+var data = require('./data/test.json');
+
 app.set('view engine','ejs');
 
 //this will allow us to serve up static files, CSS, images & JS
@@ -16,6 +19,25 @@ app.get('/', (req, res) => {
 app.get('/about-us', (req, res) => {
   var title = 'My About Us Page';
   res.render('pages/about-us',{title:title});
+});
+
+//list page of users 
+app.get('/users', function(req, res) {
+    var title = 'Users Page';
+    res.render('users/index', {
+        title: title,
+        users: data
+    });
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
 });
 
 app.get('/mission', (req, res) => {
@@ -35,4 +57,5 @@ app.get('/cards', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(data);
 });
